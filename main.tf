0da31920ff6e321ed50ddacc8b73feac361051e9
Samriddha11 <155475136+Samriddha11@users.noreply.github.com>
2026-02-21 12:58:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+provider "aws" {
+  region = var.region
+}
 
+resource "aws_instance" "ec2_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  tags = {
+    Name         = var.name
+    environment  = lower(var.environment)
+    owner        = var.owner
+    cost_centre  = var.cost_centre
+  }
+}
